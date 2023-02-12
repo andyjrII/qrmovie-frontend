d8d82724d341604b80380d430803c71dce2c65f6
AndyJrII <enehizenajames@gmail.com>
2023-02-12 13:25:34 +0100

    changed backend base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'qrmovie-backend-production.up.railway.app/';
+const BASE_URL = 'https://qr-server.onrender.com/';
 
 export default axios.create({
     baseURL: BASE_URL
